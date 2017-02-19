b2375a60bcb85e444e01cc68c020d1b20b32733c
Plamen Kolev <p.kolev22@gmail.com>
2017-02-19 17:00:32 +0000

    added admin section with password protection and authentication

--- config/initializers/rails_admin.rb
+++ config/initializers/rails_admin.rb
@@ -22,6 +22,10 @@ RailsAdmin.config do |config|
   ## == Gravatar integration ==
   ## To disable Gravatar integration in Navigation Bar set to false
   # config.show_gravatar true
+  config.authenticate_with do
+    warden.authenticate! scope: :admin
+  end
+  config.current_user_method(&:current_admin)
 
   config.actions do
     dashboard                     # mandatory
--- unicorn.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/bash
-RAILS_ENV=production bundle exec unicorn -c config/unicorn.rb
