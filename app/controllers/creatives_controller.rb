33fff3568520a961d0d0863cc9cb0f04514e9755
Plamen Kolev <p.kolev22@gmail.com>
2018-06-16 12:26:22 +0100

    min height added for better visual experience

--- app/controllers/creatives_controller.rb
+++ app/controllers/creatives_controller.rb
@@ -1,7 +1,7 @@
 class CreativesController < ApplicationController
   def index
     @artworks = Artwork.all    
-    expires_in 3.days, :public => true
-    fresh_when @artworks
+    # expires_in 3.days, :public => true
+    # fresh_when @artworks
   end
 end
--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,9 +1,9 @@
 class PagesController < ApplicationController
   def index
     @articles = Article.all
-    a = render 'pages/index'
-    expires_in 3.days, :public => true
-    fresh_when @articles
+    # a = render 'pages/index'
+    # expires_in 3.days, :public => true
+    # fresh_when @articles
   end
 
   def keybase
